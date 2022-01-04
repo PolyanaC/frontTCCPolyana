bb5181f5f2f29010d5c64b118f3094a70f174048
Polyana <polyanacsousa@gmail.com>
2022-01-03 22:35:30 -0300

    HTML e CSS de ListarPost

--- src/app/components/post/artigo-list/artigo-list.component.ts
+++ src/app/components/post/artigo-list/artigo-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-artigo-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtigoListComponent implements OnInit {
 
+  ELEMENT_DATA: Post[] = [
+    {
+    id: 1,
+    titulo: "Texto do Post",
+    texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquam lacu.",
+    createdAt:'02/03/1990',
+    updatedAt: '02/03/1990'
+    }
+  ]
+  
+  displayedColumns: string[] = ['id', 'titulo', 'texto','createdAt', 'updatedAt', 'acoes'];
+  dataSource = new MatTableDataSource<Post>(this.ELEMENT_DATA);
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
---
+++ src/app/models/post.ts
@@ -0,0 +1,7 @@
+export interface Post {
+    id: any;
+    titulo: string;
+    texto: string;
+    createdAt: any;
+    updatedAt: any;
+  }
